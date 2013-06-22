cf682d3903728de638857a0356910cee3ea233d0
Matthew Kramer <matthewkram@gmail.com>
2013-06-22 10:51:22 -0400

    changed this to self

--- server.py
+++ server.py
@@ -3,12 +3,10 @@ import tornado.web
 
 class MainRequestHandler(tornado.web.RequestHandler):
     def get(self):
-    	this.write("hello")
+    	self.write("hello")
 
 
-
-
-app = tornado.web.Application([(r"/",MainRequestHandler)])
+app = tornado.web.Application([(r"/app",MainRequestHandler)])
 
 if __name__ == "__main__":
     app.listen(80)
