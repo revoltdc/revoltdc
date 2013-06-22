6a2661ad862e4ebee88f606c3ac376a93d652123
painforever <bjslxxx@126.com>
2013-06-22 12:50:44 -0400

    put Get_Location in server.py and add list all events file
    put Get_Location in server.py and add list all events file

--- server.py
+++ server.py
@@ -1,6 +1,12 @@
 import tornado.ioloop
 import tornado.web
 from register import RegisterRequestHandler
+def Get_Location(ipv4):
+    apy_key="056a68ee3510ce527fbc9981e6860c0ba7631e8c8f489d8d841be2c196e770f7"
+    format="json"
+    request_url="http://api.ipinfodb.com/v3/ip-city/?key="+apy_key+"&ip="+ipv4+"&format="+format
+    response=urllib.urlopen(request_url)
+    return response.read()
 
 class MainRequestHandler(tornado.web.RequestHandler):
     def get(self):
