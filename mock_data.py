29836e0c2b5e87140b3e412407ac9f49ff5d65c8
painforever <bjslxxx@126.com>
2013-06-22 11:52:39 -0400

    this is some new mock data
    this is some new mock data

---
+++ mock_data.py
@@ -0,0 +1,56 @@
+﻿user=[]
+events=[]
+row1={}
+row1["first"]="Yu";
+row1["last"]="Song";
+row1["email"]="bjslxxx@126.com";
+row1["zip"]="22202";
+row1["potential"]="Republican"
+row1["Password"]="13811897631"
+
+row2={}
+row2["first"]="Mike";
+row2["last"]="Sui";
+row2["email"]="right@gmail.com";
+row2["zip"]="22202";
+row2["potential"]="Demoncratic"
+row2["Password"]="123123"
+user.append(row2)
+
+row3={}
+row3["first"]="Danny";
+row3["last"]="Andrew";
+row3["email"]="songyux6@gmail.com";
+row3["zip"]="22202";
+row3["potential"]="Republican"
+row3["Password"]="123123"
+user.append(row3)
+
+# this is events
+event1={}
+event1["name"]="Voting the new president"
+event1["time"]="2013-06-21 14:00:00"
+event1["detail"]="Let decide the future of our Country!!!!!"
+event1["loc"]="DC"
+event1["picture"]="user_photos/event1.jpg"
+events.append(event1)
+
+event2={}
+event2["name"]="Play against the incoming war!!!!"
+event2["time"]="2013-06-22 15:00:00"
+event2["detail"]="Let's fight for our country!!"
+event2["loc"]="New York"
+event2["picture"]="user_photos/event2.jpg"
+events.append(event2)
+
+event3={}
+event3["name"]="Don't enhance the Tax!!!"
+event3["time"]="2013-06-23 16:00:00"
+event3["detail"]="Let's get down the increasing tax for our people!"
+event3["loc"]="LA"
+event3["picture"]="user_photos/event3.jpg"
+events.append(event2)
+
+
+
+
