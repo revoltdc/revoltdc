fcf4ad16c3ff374f3e262ceea5ec567a5acee0c4
Matthew Kramer <matthewkram@gmail.com>
2013-06-22 12:35:59 -0400

    more changes

--- register.py
+++ register.py
@@ -1,8 +1,18 @@
 import tornado.ioloop
 import tornado.web
+from mock_data import user
 
 class RegisterRequestHandler(tornado.web.RequestHandler):
 	def get(self):
 		self.render("signup.html")
 	def post(self):
-		self.write(self.get_argument("firstname"))
+		entry = {}
+		entry["first"] = str(self.get_argument("firstname"))
+		entry["last"] = str(self.get_argument("lastname"))
+		entry["email"] = str(self.get_argument("email"))
+		entry["password"] = str(self.get_argument("password"))
+		entry["phone"] = str(self.get_argument("phone"))
+		entry["potential"] = str(self.get_argument("political"))
+		entry["zip"] = str(self.get_argument("zip"))
+		user.append(entry)
+		self.write(str(user))
--- server.py
+++ server.py
@@ -1,15 +1,17 @@
 import tornado.ioloop
 import tornado.web
 from register import RegisterRequestHandler
+import mock_data
 
 class MainRequestHandler(tornado.web.RequestHandler):
     def get(self):
-    	self.write("hello")
+    	self.render("index.html")
 
 
 
 app = tornado.web.Application([(r"/",MainRequestHandler), (r"/register", RegisterRequestHandler)])
 
 if __name__ == "__main__":
-    app.listen(80)
-    tornado.ioloop.IOLoop.instance().start()
+	print mock_data.user
+	app.listen(80)
+	tornado.ioloop.IOLoop.instance().start()
